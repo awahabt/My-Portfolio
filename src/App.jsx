a69526848fb4845993dc9ca0624c6bbdad2f8e17
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-05-23 23:29:09 +0500

    setup structure of Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Header from "./components/Header";
+import Container from "./components/Container";
+
 function App() {
   const [count, setCount] = useState(0)
 
