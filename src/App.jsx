4462b0aa431ba2d79ea286994d90039fa6c7efca
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-05-25 23:36:40 +0500

    add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Container from "./components/Container";
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +15,7 @@ function App() {
     <div className='w-full overflow-hidden'>
       <div className='px-8'>
         <div>
+          <Navbar/>
           <Header/>
         </div>
       </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between w-full'>
+      <div className=' flex'> <img src="" alt="" /></div>
+      <div>
+        <ul className='flex gap-8'>
+            <li>Projects</li>
+            <li>Github</li>
+            <li>About me</li>
+            <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
