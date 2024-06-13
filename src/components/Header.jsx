097f9b8e9cadbfe7b38183f76e8a858132a7a6e7
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-06-13 21:53:52 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import { Work, download } from "../assets";
 
 const Header = () => {
   return (
-    <div className=" text-color sidebar header z-[1] w-full">
-      <Navbar/>
-    </div>
-  )
-}
+    <div className=" text-color montserrat sidebar header z-[1] w-full">
+      <Navbar />
 
-export default Header
+      <div className="max-w-[865px]  ">
+        <p className="text-7xl font-bold  ">I’m Abdul Wahab Tahir Passionate Software Engineer</p>
+        <div className="flex flex-wrap gap-20">
+          <button className="flex gap-3 items-center ">My Work <img src={Work} alt="Work" /></button>
+          <button className="flex gap-3 items-center">Download CV <img src={download} alt="" /></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
