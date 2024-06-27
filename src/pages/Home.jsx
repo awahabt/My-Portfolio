4dd389a1dfba3455e80c3f5365c00cc5bbd9eb66
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-06-28 04:03:02 +0500

    customize  design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Work, download } from "../assets";
 
 const Home = () => {
   return (
-    <div className="max-w-[865px] h-[84vh]  flex flex-col justify-end">
+    <div className="max-w-[865px] h-[80vh]  flex flex-col justify-end">
         <p className="text-7xl font-bold text-gradient  ">I’m Abdul Wahab Tahir Passionate Software Engineer</p>
         <div className="flex flex-wrap gap-10 mt-8">
           <button className="flex gap-3 items-center work-btn ">My Work <img src={Work} alt="Work" /></button>
