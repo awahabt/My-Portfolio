d2a73de643752c8e82d3e0f89bab366af0a813a5
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-10-01 00:12:29 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import { Work, download } from "../assets";
 
 const Home = () => {
   return (
-    <div className="max-w-[1300px] ml-8 flex flex-col justify-end gap-10 max-[621px]:gap-0 min-[1605px]:pt-[400px] min-[1226px]:pt-[280px] min-[855px]:pt-[130px] max-[655px]:ml-0">
-        <p className="flex justify-center flex-col text-[110px] font-semibold text-gradient leading-[150px] max-[850px]:text-[80px] max-[850px]:leading-[110px] max-[425px]:text-[60px] max-[425px]:leading-[90px] max-[425px]:text-center max-[340px]:text-[50px]">Abdul Wahab Tahir.<br/><span className="text-2xl font-[400]">Front-End Engineer</span></p>
+    <div className="max-w-[1300px] ml-8 flex flex-col justify-end gap-10 max-[621px]:gap-0 min-[1605px]:pt-[400px] min-[1226px]:pt-[280px] min-[855px]:pt-[130px] max-[655px]:ml-0 min-[621px]:pt-[70px] min-[300px]:pt-[50px]">
+        <p className="flex justify-center flex-col text-[110px] font-semibold text-gradient leading-[150px] max-[850px]:text-[80px] max-[850px]:leading-[110px] max-[425px]:text-[60px] max-[425px]:leading-[70px] max-[425px]:text-center max-[340px]:text-[50px]">Abdul Wahab Tahir.<br/><span className="text-2xl font-[400]">Front-End Engineer</span></p>
         <div className="w-full flex flex-wrap gap-10 mt-8 max-[425px]:justify-center">
           <button className="flex gap-3 items-center work-btn ">My Work <img src={Work} alt="Work" /></button>
           <button className="flex gap-3 items-center download-btn">Download CV <img src={download} alt="" /></button>
