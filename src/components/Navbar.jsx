c6e1679e759125e8ce0b1115f7de96ba28a0b12c
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-09-30 23:19:52 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,8 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   let [active, setActive] = useState("home");
   return (
-    <div className="flex justify-between w-full items-center border">
-      <div className="pl-8 flex max-[1150px]:hidden">
+    <div className="flex justify-between w-full items-center">
+      <div className=" flex max-[1150px]:hidden">
         <ul className="flex gap-10 text-white  ">
           {NavLinks.map((links) => (
             <NavLink to={links.id} key={links.id}>
@@ -32,7 +32,7 @@ const Navbar = () => {
 
       <HiMenuAlt1 className=" min-[1150px]:hidden" />
 
-      <div className="flex gap-8 mr-10">
+      <div className="flex gap-8">
         <img src={Lightmode} alt="LightMode" className="lightmode" />
         <Button />
       </div>
