f147f92008918afd109362e235384166f9378b9d
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-09-30 23:00:50 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className=" bg-primary overflow-hidden flex justify-center">
-      <div className="w-[1700px] min-h-[90vh] px-10 border my-10">
+    <div className=" bg-primary overflow-hidden flex flex-col justify-center">
+      <div className="w-[1700px] min-h-[90vh] px-10 border mt-10">
         {/* <div className="bg-radial-gradient w-[250px] h-[250px] fixed z-[1] position blur-[100px] opacity-75" />
         <div className="bg-radial-gradient w-[250px] h-[250px] fixed z-[0] position-2 blur-[100px] opacity-30" /> */}
           <Sidebar />
@@ -14,8 +14,9 @@ function App() {
             <div><Outlet/></div>
             
           </div>
-        </div>
+        </div> 
       </div>
+     <div><p className="text-end pr-10 text-white montserrat">Made with ❤️</p></div>
     </div>
   );
 }
